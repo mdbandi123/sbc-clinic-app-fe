9d498159e936b9a9d3d84e62d8a7a35d2a449f52
Michael Dave Sumang <mdsumang.091@gmail.com>
2024-11-26 10:33:30 +0800

    initial dependency setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import WebSocketTest from './WebSocketTest'
-import useSSE from './useSSE'
+import NavDrawer from './components/NavDrawer';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
-  const [counter, setCounter] = useState<number>(0);
-  const trigger = useSSE("http://localhost:8080/api/SSE/subscribe");
-
-  useEffect(()=>{
-    console.log("hello");
-    setCounter((prev)=>{
-      return prev = prev + 1;
-    });
-  }, [trigger])
   return (
     <>
-      <h1>{counter}</h1>
+      <NavDrawer>
+        <AppRoutes/>
+      </NavDrawer>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,11 @@
+import { Routes } from "react-router";
+
+function AppRoutes(){
+  return(
+    <Routes>
+      {/* declare routes here */}
+    </Routes>
+  )
+}
+
+export default AppRoutes;
